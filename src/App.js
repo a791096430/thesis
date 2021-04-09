974ef95cc01cadbf8e634d2a93e368fcb1dd5b45
admin <ye.shu@northeastern.edu>
2021-04-09 17:25:00 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+import Navbar from "./components/navbar.component";
+import Home from "./components/home.component";
+import Search from "./components/search.component";
+import Rating from "./components/rating.component";
+import Trade from "./components/trade.component";
+import Logging from "./components/logging.component";
+import Post from "./components/post.component";
+import Profile from "./components/profile.component";
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>My react app</h1>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route path="/thesis" exact component={Home} />
+        <Route path="/search/:keyword" component={Search} />
+        <Route path="/rating/:username" component={Rating} />
+        <Route path="/trade/:username" component={Trade} />
+        <Route path="/log" component={Logging} />
+        <Route path="/detail/:id" component={Post} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </Router>
   );
 }
-
-export default App;
